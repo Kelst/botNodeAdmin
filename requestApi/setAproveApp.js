069d363e7f20347f55893c7943cfa72b35159822
VZVYN <vikt.zvynka@gmail.com>
2022-02-19 14:00:36 +0200

    s

--- requestApi/setAproveApp.js
+++ requestApi/setAproveApp.js
@@ -2,7 +2,6 @@ const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fet
 module.exports = async function (body) {
   let flag = false;
 
-<<<<<<< HEAD
    await fetch('https://guruapigamb.site/admin/api/trds3f2333/approveApp/', {
         method: 'PUT',
         body: JSON.stringify(body),
@@ -11,15 +10,5 @@ module.exports = async function (body) {
       .then(json => flag=true)
       .catch(er=>console.log(er))
       return flag
-=======
-  await fetch('https://gamblingappapi.herokuapp.com/admin/api/trds3f2333/approveApp/', {
-    method: 'PUT',
-    body: JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json' }
-  }).then(res => res.json())
-    .then(json => flag = true)
-    .catch(er => console.log(er))
-  return flag
->>>>>>> c988111b32346123d7183bebbe1ffe3a88ec6e5d
 
 }
--- requestApi/setRedirectPercent.js
+++ requestApi/setRedirectPercent.js
@@ -4,7 +4,6 @@ const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fet
 module.exports = async function (date) {
   let flag = false;
 
-<<<<<<< HEAD
    await fetch('https://guruapigamb.site/admin/api/trds3f2333/setRedirectProcent/', {
         method: 'PUT',
         body: JSON.stringify(date),
@@ -12,14 +11,5 @@ module.exports = async function (date) {
     }).then(res => res.json())
       .then(json => flag=true).catch(er=>console.log(er))
       return flag
-=======
-  await fetch('https://gamblingappapi.herokuapp.com/admin/api/trds3f2333/setRedirectProcent/', {
-    method: 'PUT',
-    body: JSON.stringify(date),
-    headers: { 'Content-Type': 'application/json' }
-  }).then(res => res.json())
-    .then(json => flag = true).catch(er => console.log(er))
-  return flag
->>>>>>> c988111b32346123d7183bebbe1ffe3a88ec6e5d
 
 } 
